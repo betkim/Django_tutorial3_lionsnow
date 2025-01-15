1cfa3cd7bc11f3bdf55a9401d161d4a321670be2
kim <ghkddus04@gmail.com>
2025-01-15 15:48:58 +0900

    s3 업뎃

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")=="True"
 
-ALLOWED_HOSTS = ['127.0.0.1'] if DEBUG else ['*'] #디버그일땐 
+# ALLOWED_HOSTS = ['127.0.0.1'] if DEBUG else ['*'] #디버그일땐 
+
+ALLOWED_HOSTS = ['*']
 
 INTERNAL_IPS = [ # ..., 
     '127.0.0.1', 
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
     'movie',
     'blog',
     'imageapp',
+    'storages',  #
 
 ]
 
@@ -152,11 +155,42 @@ USE_TZ = False
  
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [BASE_DIR/'config/static']  # 파일 경로
+STATICFILES_DIRS = [BASE_DIR/'config/static']  # 파일 경로
 STATIC_ROOT = BASE_DIR/'static'   #파이썬애니웨어 할때 필요
 
-#미디어 파일 세팅
+# STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
+AWS_STORAGE_BUCKET_NAME= os.getenv("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME=os.getenv("AWS_S3_REGION_NAME")
+AWS_S3_CUSTOM_DOMAIN=f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
+AWS_STATIC_LOCATION='static'
+AWS_MEDIA_LOCATION='media'
+AWS_ACCESS_KEY_ID=os.getenv("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY=os.getenv("AWS_SECRET_ACCESS_KEY")
+STORAGES = {
+    "default": {
+        "BACKEND": "config.storage_backends.MediaStorage",
+        "OPTIONS": {
+            "bucket_name": AWS_STORAGE_BUCKET_NAME,
+            "location": AWS_MEDIA_LOCATION,
+            "region_name": AWS_S3_REGION_NAME,
+            "custom_domain": AWS_S3_CUSTOM_DOMAIN,
+            "file_overwrite": False,
+        },
+    },
+    "staticfiles": {
+        "BACKEND": "config.storage_backends.StaticStorage",
+        "OPTIONS": {
+            "bucket_name": AWS_STORAGE_BUCKET_NAME,
+            "location": AWS_STATIC_LOCATION,
+            "region_name": AWS_S3_REGION_NAME,
+            "custom_domain": AWS_S3_CUSTOM_DOMAIN
+        },
+    },
+}
+
+#미디어 파일 세팅
+# MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
